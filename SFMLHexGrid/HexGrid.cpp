9d64a06e9f0160fdae8afd47783be06909865ec6
jonhy31 <lockedandloadedgames@gmail.com>
2013-11-20 17:16:05 +0000

    Update HexGrid.cpp

--- SFMLHexGrid/HexGrid.cpp
+++ SFMLHexGrid/HexGrid.cpp
@@ -8,8 +8,8 @@ HexGrid::HexGrid(int height, int width, float radius, sf::Color color){
 	defColor = color;
 	hexNumber = width * height;
 	yRow = 1;
-    	xKeyPos = 0;
-    	yKeyPos = 0;
+    xKeyPos = 0;
+   	yKeyPos = 0;
 
 }
 
@@ -18,8 +18,8 @@ void HexGrid::draw(sf::RenderWindow &App){
 	hexagon.setRadius(defRadius);
 	hexagon.setPointCount(6);
 	hexagon.setFillColor(defColor);
-    	hexagon.setOutlineThickness(1);
-    	hexagon.setOutlineColor(sf::Color::Black);
+    hexagon.setOutlineThickness(1);
+    hexagon.setOutlineColor(sf::Color::Black);
 
 	xPos = hexagon.getRadius();
 	yPos = hexagon.getRadius();
@@ -28,25 +28,25 @@ void HexGrid::draw(sf::RenderWindow &App){
 		
 
 		hexagon.setPosition(xPos, yPos);
-        	hexBounds = hexagon.getLocalBounds();
+        hexBounds = hexagon.getLocalBounds();
 
 
 
 		if( i % defWidth == 0 && i != 0 ){
 			if (yRow % 2 == 0){
-                		xPos = hexagon.getRadius();
-                		yPos += hexagon.getRadius() * 1.55;
+                xPos = hexagon.getRadius();
+                yPos += hexagon.getRadius() * 1.55;
 			}
 			else{
-                		xPos = sqrt(3) * hexagon.getRadius() + ((hexagon.getRadius() / 10) + 1.3 );
-        			yPos += hexagon.getRadius() * 1.55;
+                xPos = sqrt(3) * hexagon.getRadius() + ((hexagon.getRadius() / 10) + 1.3 );
+        		yPos += hexagon.getRadius() * 1.55;
 			}
 
 			++yRow;
 		}
 
 		else
-            		xPos += sqrt(3) * hexagon.getRadius() + 0.5;
+            xPos += sqrt(3) * hexagon.getRadius() + 0.5;
 
         	App.draw(hexagon);
 
